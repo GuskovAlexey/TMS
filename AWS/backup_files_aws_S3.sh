884923ab8f7cc84c4cf14d6fb21f584ae3dba65a
GuskovAlexey <guskovpln@gmail.com>
2022-12-28 15:46:35 +0100

    Add AWS S3 backup files

---
+++ AWS/backup_files_aws_S3.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+##   $crontab -e
+##   Dayly backup, at 12:00 pm
+##   0 0 * * * /home/lespot/script/backup_files_aws_S3.sh > /dev/null 2>&1
+
+# Basic configuration: datestamp e.g. YYYYMMDD
+DATE=$(date +"%Y-%m-%d-%H:%M")
+
+# What folder backup
+
+BACKUP_FOLDER="/var/www/html/"
+
+mkdir tmp/backup
+cp $BACKUP_FOLDER $TMP_BACKUP_DIR
+
+# Location of your backups 
+TMP_BACKUP_DIR="/tmp/backup"
+BACKUP_DIR="${TMP_BACKUP_DIR}/${DATE}"
+
+# Create a new directory into backup directory location for this date
+mkdir -p $BACKUP_DIR
+
+# -------------------------------
+# ---- BEGIN UPLOADING TO S3 ----
+# -------------------------------
+BACKUP_FILE="${TMP_BACKUP_DIR}/${DATE}.tar.gz"
+tar czf $BACKUP_FILE -P $BACKUP_DIR
+
+S3_BUCKET="s3://test-dos11/Test/"
+
+echo $(date +"%b %d, %Y %H:%M:%S Begin uploading $BACKUP_FILE to $S3_BUCKET")
+aws s3 cp $BACKUP_FILE $S3_BUCKET
+echo $(date +"%b %d, %Y %H:%M:%S End uploading to S3")
+
+# Remove backup files after uploading to S3
+echo $(date +"%b %d, %Y %H:%M:%S Remove $TMP_BACKUP_DIR after uploading to S3")
+#rm -rf $TMP_BACKUP_DIR
+
+# --------------------------------------
+# ---- DELETE OLD BACKUP FILE IN S3 ----
+# --------------------------------------
+flist=($(aws s3 ls ${S3_BUCKET} | awk '{print $4}'))
+len=${#flist[@]}
+
+# Loop to delete all files and keep 3 latest files
+for ((i = 0; i < $((len-3)); i++)); do
+    ele0="${S3_BUCKET}${flist[$i]}"
+    echo $(date +"%b %d, %Y %H:%M:%S Remove ${ele0}")
+    aws s3 rm ${ele0}
+done
